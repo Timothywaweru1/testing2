bd913ae22c1f73f5d668c42fc095b42e2ac3c7bb
Timothy <timothy.waweru1@student.moringaschool.com>
2025-05-07 20:17:55 +0300

    testing dicts

--- dict/dict.py
+++ dict/dict.py
@@ -19,3 +19,49 @@ capitals.popitem()
 keys = capitals.keys()
 for key in capitals :
     print(key)
+
+#dict mapping
+day_map = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+}
+day_map_no = 6
+print(day_map.get(day_map_no, "invalid day"))
+
+
+
+#mapping into funcs
+def mul (a,b) :
+    return a * b
+def div (a,b) :
+    return a / b
+def add (a,b) :
+    return a + b
+def sub (a,b) :
+    return a - b
+
+dict_sol = {
+    "ad" : add,
+    "subtract" : sub,
+    "multiply" : mul,
+    "divide" : div,
+}
+want_required_sol = "divide"
+result = dict_sol.get("divide")(100,5)
+print(result)
+
+
+math_map = {
+    "double" : lambda x : x * 2,
+    "square" : lambda x : x ** 2,
+    "negate" : lambda x : -x,
+}
+op = "square"
+number = 5
+result = math_map[op](number)
+print(result)
