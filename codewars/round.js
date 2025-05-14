050bfd725449da62ef524b51f6c85b487cb2dba8
Timothy <timothy.waweru1@student.moringaschool.com>
2025-05-14 21:21:12 +0300

    wars

---
+++ codewars/round.js
@@ -0,0 +1,13 @@
+
+
+
+function litres(time) {
+    const lt = 0.5 * time
+    let result = Math.floor(lt)
+    console.log(result);
+    if(lt === lt){
+        return 0;
+    }
+    
+}
+litres(11.8)
