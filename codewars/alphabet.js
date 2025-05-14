e0ba25257ed8d33ffa3adc7ab3b7bb43fb343f46
Timothy <timothy.waweru1@student.moringaschool.com>
2025-05-14 23:44:15 +0300

    wars

---
+++ codewars/alphabet.js
@@ -0,0 +1,10 @@
+function alphabetPosition(text) {
+    const brk = text.split("")
+    console.log(brk);
+    if("a b c d e f g h i j k l m n o p q r s t u v w x y z".includes(text)){
+
+    }
+    
+    return text;
+}
+alphabetPosition("The sunset sets at twelve o' clock")
